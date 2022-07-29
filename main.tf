4953ba8037d7017d1b4da3d00c6a078f102e4e68
Quentin RICHARD <quentinr@padok.fr>
2022-07-29 14:44:48 +0200

    fix(data): remove data from module

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
-# Data
-data "azurerm_resource_group" "this" {
-  name = var.resource_group_name
-}
-
-data "azurerm_client_config" "this" {}
 
 # Resource : azurerm_key_vault
 resource "azurerm_key_vault" "this" {
   name                            = var.name
-  location                        = data.azurerm_resource_group.this.location
-  resource_group_name             = data.azurerm_resource_group.this.name
+  location                        = var.resource_group.location
+  resource_group_name             = var.resource_group.name
   sku_name                        = var.sku_name
-  tenant_id                       = data.azurerm_client_config.this.tenant_id
+  tenant_id                       = var.tenant_id
   enabled_for_deployment          = var.enabled_for_deployment
   enabled_for_disk_encryption     = var.enabled_for_disk_encryption
   enabled_for_template_deployment = var.enabled_for_template_deployment
@@ -30,7 +24,7 @@ resource "azurerm_key_vault" "this" {
   dynamic "access_policy" {
     for_each = var.access_policy
     content {
-      tenant_id               = data.azurerm_client_config.this.tenant_id
+      tenant_id               = var.tenant_id
       object_id               = access_policy.key
       application_id          = access_policy.value.application_id
       certificate_permissions = access_policy.value.certificate_permissions
@@ -41,40 +35,4 @@ resource "azurerm_key_vault" "this" {
   }
 
   tags = var.tags
-}
-data "azurerm_monitor_diagnostic_categories" "this" {
-  count       = var.logs_enabled ? 1 : 0
-  resource_id = azurerm_key_vault.this.id
-}
-resource "azurerm_monitor_diagnostic_setting" "this" {
-  count              = var.logs_enabled ? 1 : 0
-  name               = "Diagnostic-${var.name}"
-  target_resource_id = azurerm_key_vault.this.id
-
-  log_analytics_workspace_id     = var.log_analytics_workspace_id
-  storage_account_id             = var.storage_account_id
-  eventhub_authorization_rule_id = var.eventhub_authorization_rule_id
-
-  log_analytics_destination_type = null
-
-  dynamic "log" {
-    for_each = data.azurerm_monitor_diagnostic_categories.this[count.index].logs
-    content {
-      category = log.value
-      retention_policy {
-        days    = 0
-        enabled = false
-      }
-    }
-  }
-  dynamic "metric" {
-    for_each = data.azurerm_monitor_diagnostic_categories.this[count.index].metrics
-    content {
-      category = metric.value
-      retention_policy {
-        days    = 0
-        enabled = false
-      }
-    }
-  }
 }
