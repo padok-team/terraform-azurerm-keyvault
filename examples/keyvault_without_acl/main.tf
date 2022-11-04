87ce10adbc51424698282f9febc4ef49daffb3e7
Pierre Averous <piaverous@gmail.com>
2022-11-04 14:43:31 +0100

    doc(example): add example for keyvault without network ACLs

---
+++ examples/keyvault_without_acl/main.tf
@@ -0,0 +1,39 @@
+# Short description of the use case in comments
+
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.82.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.4"
+    }
+  }
+}
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  features {}
+}
+
+resource "random_pet" "random" {}
+
+data "azurerm_client_config" "this" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "test-keyvault"
+  location = "francecentral"
+}
+
+module "keyvault" {
+  source = "../.."
+  name   = random_pet.random.id # Vault names are globaly unique
+
+  resource_group     = azurerm_resource_group.example
+  tenant_id          = data.azurerm_client_config.this.tenant_id
+  enable_network_acl = false
+
+  sku_name = "standard"
+}
